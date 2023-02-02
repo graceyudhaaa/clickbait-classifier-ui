4f2408edca53b41641a42a45c6ba73f6bdc18c5c
graceyudhaaa <emmanuelgrace005@gmail.com>
2023-02-03 05:04:20 +0700

    update model and threshold

--- sites/app/blueprint/api/controllers.py
+++ sites/app/blueprint/api/controllers.py
@@ -20,49 +20,50 @@ api = Blueprint(
 )
 
 text_cleaning_dict = {
-    "tokenizer_stopword_in_not_stemmed"         : text_cleaning_stopword_in_not_stemmed,
-    "tokenizer_stopword_in_stemmed"             : text_cleaning_stopword_in_stemmed,
-    "tokenizer_stopword_removed_not_stemmed"    : text_cleaning_stopword_removed_not_stemmed,
-    "tokenizer_stopword_removed_stemmed"        : text_cleaning_stopword_removed_stemmed
+    "tokenizer_stopword_in_not_stemmed": text_cleaning_stopword_in_not_stemmed,
+    "tokenizer_stopword_in_stemmed": text_cleaning_stopword_in_stemmed,
+    "tokenizer_stopword_removed_not_stemmed": text_cleaning_stopword_removed_not_stemmed,
+    "tokenizer_stopword_removed_stemmed": text_cleaning_stopword_removed_stemmed
 }
 
 model_threshold = {
-    'model_stopword_in_not_stemmed_bidirectional_keras_embedding'         : 0.480511,
-    'model_stopword_in_not_stemmed_bidirectional_word2vec'                : 0.422630,
-    'model_stopword_in_not_stemmed_non_bidirectional_keras_embedding'     : 0.445286,
-    'model_stopword_in_not_stemmed_non_bidirectional_word2vec'            : 0.333427,
-    'model_stopword_in_stemmed_bidirectional_keras_embedding'             : 0.491242,
-    'model_stopword_in_stemmed_bidirectional_word2vec'                    : 0.272098,
-    'model_stopword_in_stemmed_non_bidirectional_keras_embedding'         : 0.413950,
-    'model_stopword_in_stemmed_non_bidirectional_word2vec'                : 0.357529,
-    'model_stopword_removed_not_stemmed_bidirectional_keras_embedding'    : 0.412049,
-    'model_stopword_removed_not_stemmed_bidirectional_word2vec'           : 0.394056,
-    'model_stopword_removed_not_stemmed_non_bidirectional_keras_embedding': 0.473019,
-    'model_stopword_removed_not_stemmed_non_bidirectional_word2vec'       : 0.384295,
-    'model_stopword_removed_stemmed_bidirectional_keras_embedding'        : 0.491892,
-    'model_stopword_removed_stemmed_bidirectional_word2vec'               : 0.307583,
-    'model_stopword_removed_stemmed_non_bidirectional_keras_embedding'    : 0.479575,
-    'model_stopword_removed_stemmed_non_bidirectional_word2vec'           : 0.428392,  
+    'model_stopword_in_not_stemmed_bidirectional_keras_embedding': 0.324794,
+    'model_stopword_in_not_stemmed_bidirectional_word2vec': 0.465115,
+    'model_stopword_in_not_stemmed_non_bidirectional_keras_embedding': 0.365479,
+    'model_stopword_in_not_stemmed_non_bidirectional_word2vec': 0.310083,
+    'model_stopword_in_stemmed_bidirectional_keras_embedding': 0.337279,
+    'model_stopword_in_stemmed_bidirectional_word2vec': 0.242802,
+    'model_stopword_in_stemmed_non_bidirectional_keras_embedding': 0.453993,
+    'model_stopword_in_stemmed_non_bidirectional_word2vec': 0.452290,
+    'model_stopword_removed_not_stemmed_bidirectional_keras_embedding': 0.492096,
+    'model_stopword_removed_not_stemmed_bidirectional_word2vec': 0.476125,
+    'model_stopword_removed_not_stemmed_non_bidirectional_keras_embedding': 0.367642,
+    'model_stopword_removed_not_stemmed_non_bidirectional_word2vec': 0.340811,
+    'model_stopword_removed_stemmed_bidirectional_keras_embedding': 0.486688,
+    'model_stopword_removed_stemmed_bidirectional_word2vec': 0.376050,
+    'model_stopword_removed_stemmed_non_bidirectional_keras_embedding': 0.498264,
+    'model_stopword_removed_stemmed_non_bidirectional_word2vec': 0.352744,
 }
 
 word2vec_dict = {
-    'idwiki_100_cbow'     : 'idwiki_word2vec_100_new_lower.model',
-    'idwiki_200_cbow'     : 'idwiki_word2vec_200_new_lower.model',
-    'leizig_100_cbow'     : 'word2vec100_leipzig_stopword_in_not_stemmed_cbow.model',
-    'leipzig_100_skipgram' : 'word2vec100_leipzig_stopword_in_not_stemmed_skip-gram.model',
-    'leipzig_200_cbow'     : 'word2vec200_leipzig_stopword_in_not_stemmed_cbow.model',
-    'leipzig_200_skipgram' : 'word2vec200_leipzig_stopword_in_not_stemmed_skip-gram.model',
-    'leipzig_300_cbow'     : 'word2vec300_leipzig_stopword_in_not_stemmed_cbow.model',
-    'leipzig_300_skipgram' : 'word2vec300_leipzig_stopword_in_not_stemmed_skip-gram.model',
+    'idwiki_100_cbow': 'idwiki_word2vec_100_new_lower.model',
+    'idwiki_200_cbow': 'idwiki_word2vec_200_new_lower.model',
+    'leizig_100_cbow': 'word2vec100_leipzig_stopword_in_not_stemmed_cbow.model',
+    'leipzig_100_skipgram': 'word2vec100_leipzig_stopword_in_not_stemmed_skip-gram.model',
+    'leipzig_200_cbow': 'word2vec200_leipzig_stopword_in_not_stemmed_cbow.model',
+    'leipzig_200_skipgram': 'word2vec200_leipzig_stopword_in_not_stemmed_skip-gram.model',
+    'leipzig_300_cbow': 'word2vec300_leipzig_stopword_in_not_stemmed_cbow.model',
+    'leipzig_300_skipgram': 'word2vec300_leipzig_stopword_in_not_stemmed_skip-gram.model',
 }
 
-####### text cleaning function ####### 
+####### text cleaning function #######
+
 
 def text_cleaning_stopword_in_not_stemmed_api(text):
     return_dict = {
         'text': text,
     }
-    
+
     text = text.lower().encode('ascii',  "ignore").decode('utf-8')
     return_dict['casefolding'] = text
 
@@ -77,11 +78,12 @@ def text_cleaning_stopword_in_not_stemmed_api(text):
 
     return return_dict, text
 
+
 def text_cleaning_stopword_in_stemmed_api(text):
     return_dict = {
         'text': text,
     }
-    
+
     text = text.lower().encode('ascii',  "ignore").decode('utf-8')
     return_dict['casefolding'] = text
 
@@ -98,6 +100,7 @@ def text_cleaning_stopword_in_stemmed_api(text):
     return_dict['stemming'] = text
 
     return return_dict, text
+
 
 def text_cleaning_stopword_removed_not_stemmed_api(text):
     return_dict = {
@@ -120,6 +123,7 @@ def text_cleaning_stopword_removed_not_stemmed_api(text):
     return_dict['remove_stopword'] = text
 
     return return_dict, text
+
 
 def text_cleaning_stopword_removed_stemmed_api(text):
     return_dict = {
@@ -146,19 +150,22 @@ def text_cleaning_stopword_removed_stemmed_api(text):
 
     return return_dict, text
 
+
 preprocessing_api_dict = {
-    "tokenizer_stopword_in_not_stemmed"         : text_cleaning_stopword_in_not_stemmed_api,
-    "tokenizer_stopword_in_stemmed"             : text_cleaning_stopword_in_stemmed_api,
-    "tokenizer_stopword_removed_not_stemmed"    : text_cleaning_stopword_removed_not_stemmed_api,
-    "tokenizer_stopword_removed_stemmed"        : text_cleaning_stopword_removed_stemmed_api
+    "tokenizer_stopword_in_not_stemmed": text_cleaning_stopword_in_not_stemmed_api,
+    "tokenizer_stopword_in_stemmed": text_cleaning_stopword_in_stemmed_api,
+    "tokenizer_stopword_removed_not_stemmed": text_cleaning_stopword_removed_not_stemmed_api,
+    "tokenizer_stopword_removed_stemmed": text_cleaning_stopword_removed_stemmed_api
 }
 
-#======== END model related helper function ========
+# ======== END model related helper function ========
+
 
 @api.route('/api/test')
 @cross_origin()
 def api_test():
     return jsonify('hello world')
+
 
 @api.route('/api/predict', methods=['POST'])
 @cross_origin()
@@ -166,11 +173,13 @@ def predict():
     start = datetime.now()
     data = request.json                         # get data from request
 
-    text      = data['text']                    # get text
-    stopword  = data['stopword']                # get the stopword parameter
-    stemming  = data['stemming']                # get the stemming parameter
-    lstm_mode = data['lstm']                    # get the lstm layer type parameter
-    embedding = data['embedding']               # get the embedding type parameter
+    text = data['text']                    # get text
+    stopword = data['stopword']                # get the stopword parameter
+    stemming = data['stemming']                # get the stemming parameter
+    # get the lstm layer type parameter
+    lstm_mode = data['lstm']
+    # get the embedding type parameter
+    embedding = data['embedding']
 
     tokenizer_name = "_".join(['tokenizer', stopword, stemming])
     tokenizer_path = f"sites/app/tokenizers/{tokenizer_name}.json"
@@ -193,17 +202,18 @@ def predict():
     score = model.predict(sequence).flatten()[0]
 
     response = {
-                "model"                 : model_name,
-                "score"                 : float(score),
-                "tokenizer"             : tokenizer_name,
-                "text_cleaning_func"    : tokenizer_name.replace("tokenizer", "text_cleaning"),
-                "runtime"               : float((datetime.now() - start).total_seconds()),
-                'result'                : 0 if score < 0.5 else 1,
-                'result_best_threshold' : 0 if score < best_threshold else 1,
-                'best_threshold'        : best_threshold
-               }
+        "model": model_name,
+        "score": float(score),
+        "tokenizer": tokenizer_name,
+        "text_cleaning_func": tokenizer_name.replace("tokenizer", "text_cleaning"),
+        "runtime": float((datetime.now() - start).total_seconds()),
+        'result': 0 if score < 0.5 else 1,
+        'result_best_threshold': 0 if score < best_threshold else 1,
+        'best_threshold': best_threshold
+    }
 
     return jsonify(response)
+
 
 @api.route('/api/preprocess', methods=['POST'])
 @cross_origin()
@@ -211,9 +221,9 @@ def preprocess():
     start = datetime.now()
     data = request.json                         # get data from request
 
-    text      = data['text']                    # get text
-    stopword  = data['stopword']                # get the stopword parameter
-    stemming  = data['stemming']                # get the stemming parameter
+    text = data['text']                    # get text
+    stopword = data['stopword']                # get the stopword parameter
+    stemming = data['stemming']                # get the stemming parameter
 
     tokenizer_name = "_".join(['tokenizer', stopword, stemming])
     tokenizer_path = f"sites/app/tokenizers/{tokenizer_name}.json"
@@ -221,7 +231,7 @@ def preprocess():
     text_cleaning_func = preprocessing_api_dict[tokenizer_name]
 
     response, text_preprocessed = text_cleaning_func(text)
-    
+
     sequence = tokenizer.texts_to_sequences([text_preprocessed])
     sequence = pad_sequences(sequence, maxlen=30)
 
@@ -229,13 +239,14 @@ def preprocess():
 
     return jsonify(response)
 
+
 @api.route('/api/word2vec_sim', methods=['POST'])
 @cross_origin()
 def word2vec_sim():
     data = request.json                         # get data from request
 
-    text      = data['text'].lower().split()    # get text
-    corpus    = data['corpus']                  # get corpus
+    text = data['text'].lower().split()    # get text
+    corpus = data['corpus']                  # get corpus
     dimension = str(data['dimension'])          # get the embedding dimension
     algorithm = data['algorithm']               # get the training algorithm
 
@@ -248,16 +259,20 @@ def word2vec_sim():
 
     return jsonify(result)
 
+
 @api.route('/api/word2vec_sim_cosmul', methods=['POST'])
 @cross_origin()
 def word2vec_sim_cosmul():
     data = request.json                         # get data from request
 
     text_positive = data['text_positive'].lower().split()    # get positve text
-    text_negative = data['text_negative'].lower().split()    # get negative text
-    corpus        = data['corpus']                           # get corpus
-    dimension     = str(data['dimension'])                   # get the embedding dimension
-    algorithm     = data['algorithm']                        # get the training algorithm
+    text_negative = data['text_negative'].lower(
+    ).split()    # get negative text
+    corpus = data['corpus']                           # get corpus
+    # get the embedding dimension
+    dimension = str(data['dimension'])
+    # get the training algorithm
+    algorithm = data['algorithm']
 
     model_name = word2vec_dict['_'.join([corpus, dimension, algorithm])]
     model_path = f'sites/app/word2vec/{model_name}'
