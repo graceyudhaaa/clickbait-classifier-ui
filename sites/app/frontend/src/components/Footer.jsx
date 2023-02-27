f3eab0cdaa547227213b6cec4b5db6c95f0141b6
graceyudhaaa <emmanuelgrace005@gmail.com>
2023-02-27 20:50:00 +0700

    update footer

--- sites/app/frontend/src/components/Footer.jsx
+++ sites/app/frontend/src/components/Footer.jsx
@@ -12,8 +12,21 @@ function Footer() {
   return (
     <Box sx={{ bgcolor: "#1e1e1e", color: "white", mt: 6 }}>
       <Container maxWidth="md" sx={{ py: 6 }}>
-        <Stack spacing={4}>
-          <Typography variant="caption">Grace Yudha Satriawan</Typography>
+        <Stack spacing={2}>
+          <Typography variant="h6">Tentang Clickbait</Typography>
+          <Typography align="justify" variant="caption">
+            Clickbait adalah konten darisebuah website yang sengaja dibuat dan
+            didesain untuk membuat pembaca yang tertarik untuk mengeklik tautan
+            yang ditampilkan, biasanya konten-konten clickbait disebarkan pada
+            media sosial. Clickbait juga dapat menyebabkan kualitas dari
+            jurnalisme yang menurun karena lebih mementingkan judul yang lebih
+            meninggikan jumlah pengunjung dibandingkan dengan informasi yang
+            dikandung oleh artikel berita itu sendiri. Hal ini juga dapat memicu
+            penyebaran berita hoaks di masyarakat, yang dapat menimbulkan
+            keributan di masyarakat. Sehingga diperlukan cara untuk
+            mengklasifikasi clickbait agar dapat mencegah dampak-dampak negatif
+            dari clickbait.
+          </Typography>
         </Stack>
       </Container>
     </Box>
